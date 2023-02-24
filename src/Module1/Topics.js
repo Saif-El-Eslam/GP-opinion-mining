bd2e976799b2328331f3fd548faeac1e9331f758
AhmedYasser155 <75222161+AhmedYasser155@users.noreply.github.com>
2023-02-24 10:23:06 +0200

    Static real data added

--- src/Module1/Topics.js
+++ src/Module1/Topics.js
@@ -1,33 +1,30 @@
-import './topics.css';
-import Topic from './topic';
+import "./topics.css";
+import Topic from "./topic";
 
-import { useState } from 'react';
-
+import { useState } from "react";
 
 function Topics() {
-
-
-    const [topics, setTopics] = useState([
-        "Topic 1",
-        "Topic 2",
-        "Topic 3",
-        "Topic 4",
-        "Topic 5",
-        "Topic 6",
-        "Topic 7",
-    ]);
+  const [topics, setTopics] = useState([
+    "Russia’s Brutal and Unprovoked Invasion of Ukraine",
+    "Topic 2",
+    "Topic 3",
+    "Topic 4",
+    "Topic 5",
+    "Topic 6",
+    "Topic 7",
+  ]);
 
   return (
     <div className="topics">
-        <div className='topics-header'>
-            <h1>Topics</h1>
-        </div>
-        
-        <div className='topics-content'>
-            {topics.map((topic, i) => (
-                <Topic key={i} topic={topic}/>
-            ))}
-        </div>
+      <div className="topics-header">
+        <h1>Topics</h1>
+      </div>
+
+      <div className="topics-content">
+        {topics.map((topic, i) => (
+          <Topic key={i} topic={topic} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Module2/Opinion.js
+++ src/Module2/Opinion.js
@@ -1,34 +1,45 @@
-import './opinion.css';
+import "./opinion.css";
 
 import { Link } from "react-router-dom";
 
-
-function Opinion({topic, person}) {
+function Opinion({ topic, person }) {
   return (
-    <div className='opinion'>
-        <div className='opinion-wrapper'>
-            <h1 className='opinion-header'> Here is {person||'"person"'}'s opinion about {topic||'"topic"'}. </h1>
+    <div className="opinion">
+      <div className="opinion-wrapper">
+        <h1 className="opinion-header">
+          {" "}
+          Here is {person || '"person"'}'s opinion about{" "}
+          {topic || "Russia’s Brutal and Unprovoked Invasion of Ukraine"}.{" "}
+        </h1>
 
-            <div className='opinion-content'>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliq
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliq
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliq
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliq
-                </p>
-            </div>
+        <div className="opinion-content">
+          <p>
+            In the first anniversary of the Russian invasion of Ukraine, Joe
+            Biden has called for a “stronger response” to Russia’s aggression.
+            The United States and its allies must stand up to Russia’s
+            aggression and support Ukraine’s sovereignty and territorial
+            integrity.
+          </p>
+        </div>
+      </div>
+
+      <div className="opinion-wrapper">
+        <h1 className="opinion-header"> Compare with your opinion </h1>
+
+        <div className="opinion-content">
+          <textarea
+            id="compare-opinion-input"
+            type="text"
+            placeholder="Your Opinion"
+          ></textarea>
         </div>
 
-        <div className='opinion-wrapper'>
-            <h1 className='opinion-header'> Compare with your opinion </h1>
-
-            <div className='opinion-content'>
-                <textarea id='compare-opinion-input' type="text" placeholder="Your Opinion"></textarea>
-            </div>
-
-            <Link className='margin-top-down-50' to="/comparison-result"> <button id='compare-button'>Compare</button> </Link>
-        </div>
-    </div>  
+        <Link className="margin-top-down-50" to="/comparison-result">
+          {" "}
+          <button id="compare-button">Compare</button>{" "}
+        </Link>
+      </div>
+    </div>
   );
 }
 
